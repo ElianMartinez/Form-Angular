adf74a48138832ec3cdf42bf9bbe02b7e6e8d265
Elian Martinez <ElianMartinez157@gmail.com>
2019-11-17 18:17:36 -0400

    Agregando componentes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormDataService } from '../data/formData.service';
 import { Personal } from '../data/formData.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -10,11 +11,11 @@ import { Personal } from '../data/formData.model';
 })
 export class FormComponent implements OnInit {
   personal: Personal;
-  form: any;
+    form: any;
   
    step:string;
    title = 'Multi-Step Wizard';
-   actPersonal:boolean = false;
+   actPersonal:boolean = true;
    actFamiliar:boolean = false;
    actEducacion:boolean = false;
    actConTec:boolean = false;
@@ -25,14 +26,96 @@ export class FormComponent implements OnInit {
   
    @Input() formData;
     
-    constructor(private formDataService: FormDataService, private rutaActiva: ActivatedRoute, ) {
+    constructor(private formDataService: FormDataService, private rutaActiva: ActivatedRoute, private router: Router) {
 
     }
+
+    resertStep(){
+     this.actPersonal = false;
+      this.actFamiliar = false;
+      this.actEducacion = false;
+      this.actConTec = false;
+      this.actIdioma = false;  
+     this.actExpLaboral = false;
+      this.actRefPersonal = false;
+     this.actAtExtra = false;
+    }
+
+    activarStep(num:number, form:any){
+      switch (num) {
+        
+        case 1:
+           
+                this.resertStep();
+                this.actPersonal = true;
+                this.next(num,form);
+                 break;
+        case 2:
+                this.resertStep();
+                this.actFamiliar = true;
+                this.next(num,form);
+                 break;
+        case 3:
+               this.resertStep();
+                this.actEducacion = true;
+                this.next(num,form);
+                 break;
+        case 4:
+            this.resertStep();
+            this.actConTec = true;
+            this.next(num,form);
+             break;
+        case 5:
+            this.resertStep();
+                this.actIdioma = true;
+                this.next(num,form);
+                 break;
+        case 6:
+            this.resertStep();
+            this.actExpLaboral = true;
+            this.next(num,form);
+             break;
+        case 7:
+            this.resertStep();
+            this.actRefPersonal = true;
+            this.next(num,form);
+             break;
+
+        case 8:
+            this.resertStep();
+            this.actAtExtra = true;
+            this.next(num,form);
+            break;
+       }
+
+    }
+
+   next(num:number,form: any){
+    if (this.save(form)) {
+      // Navigate to the work page
+      this.router.navigate([`/form/${num}`]);
+     
+  }
+  
+   }
+      
+
+   save(form: any): boolean {
+    if (!form.valid) {
+        return false;
+    }
+        
+    this.formDataService.setPersonal(this.personal);
+    return true;
+}
+
  
     ngOnInit() {
+        this.personal = this.formDataService.getPersonal();
+        
         this.formData = this.formDataService.getFormData();
         this.step = this.rutaActiva.snapshot.params.number;
-        console.log(this.formData);
+  
         
 
 }
